ad107841b76d9b2437b062175901cbd271c0e2a7
CREWMATE <CREWMATE@DESKTOP-EEJ42B1>
2020-10-21 17:44:07 +0900

    백준 다이나믹 프로그래밍 문제 풀이

--- Begin/src/intern12/BaekJoon05.java
+++ Begin/src/intern12/BaekJoon05.java
@@ -4,51 +4,37 @@ import java.util.Scanner;
 
 public class BaekJoon05 {
 
+    static int[] d;
     public static void main(String[] args) {
         // 1463번 1로 만들기 //
         Scanner sc = new Scanner(System.in);
-        long num = sc.nextInt();
+        int num = sc.nextInt();
+        d = new int[num+1];
+        System.out.println(dp(num));
         sc.close();
-        long count = 0;
-        long min = 0;
-        long temp = num;
-        while (temp > 1) {
-            if (temp % 3 == 0) {
-                count++;
-                temp = temp / 3;
-            } else {
-                count++;
-                temp -= 1;
-            }
-            if (temp % 2 == 0) {
-                count++;
-                temp = temp / 2;
-            }
-
+    }
+    
+    private static int dp(int x) {
+        
+        if(x==1)
+            return 0;
+        if(d[x] > 0)
+            return d[x];
+        d[x] = dp(x-1)+1; //d[x]는 x-1의 최소횟수 +1
+        
+        // x가 나눠지는지 확인 후 나눠지면 (나눈 몫의 최소 횟수 +1)과 (x-1의 최소횟수 +1) 비교 후 작은 값 대입
+        if(x%2==0) {
+            int tmp = dp(x/2)+1;
+            if(d[x] > tmp)
+                d[x] = tmp;
         }
-        min = count;
-        temp = num;
-        count = 0;
-        while (temp > 1) {
-            if (temp % 2 == 0) {
-                count++;
-                temp = temp / 2;
-            } else {
-                count++;
-                temp -= 1;
-            }
-            if (temp % 3 == 0) {
-                count++;
-                temp = temp / 3;
-            }
-
+            
+        if(x%3==0) {
+            int tmp = dp(x/3)+1;
+            if(d[x] > tmp)
+                d[x] = tmp;
         }
-
-        if (min > count)
-            min = count;
-
-        System.out.println(min);
-
+        return d[x];
     }
 
 }
